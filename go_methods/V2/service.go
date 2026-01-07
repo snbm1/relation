6fca255ba8d8e88d4cb9a80c57de108a6c790481
ZennardOS <shadowkod2006@gmail.com>
2026-01-07 17:42:12 +0300

    completed the service.go
    The problem remains that the service.go file pulls other functions and variables from third-party files.

--- go_methods/V2/service.go
+++ go_methods/V2/service.go
@@ -1,23 +1,30 @@
 package v2
 
 import (
+	"context"
 	"io"
+	"methods/manager"
 	"os"
 	"runtime"
+	runtimeDebug "runtime/debug"
 	"time"
 
-	"methods/manager"
-
+	SingBox "github.com/sagernet/sing-box"
+	"github.com/sagernet/sing-box/common/urltest"
 	"github.com/sagernet/sing-box/experimental/libbox"
 	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
 	Ex "github.com/sagernet/sing/common/exceptions"
+	"github.com/sagernet/sing/service"
+	"github.com/sagernet/sing/service/filemanager"
+	"github.com/sagernet/sing/service/pause"
 )
 
 var (
 	thisworkingPath           string
 	thisTempPath              string
-	thisUserID                int
-	thisGroupID               int
+	UserID                    int
+	GroupID                   int
 	thisstatusPropagationPort int64
 	coreLogFactory            log.Factory //Исправить, дописать файл который будет его импортировать!!!
 )
@@ -33,8 +40,8 @@ func Setup(basicPath, workingPath, tempPath string, statusPort int64, debug, ena
 	thisworkingPath = workingPath
 	thisTempPath = tempPath
 	os.Chdir(thisworkingPath)
-	thisUserID = os.Getuid()
-	thisGroupID = os.Getgid()
+	UserID = os.Getuid()
+	GroupID = os.Getgid()
 
 	var defaultWriter io.Writer
 	if !debug {
@@ -58,4 +65,40 @@ func Setup(basicPath, workingPath, tempPath string, statusPort int64, debug, ena
 	}
 
 	return nil
+}
+
+func NewService(options option.Options) (*libbox.BoxService, error) {
+	runtimeDebug.FreeOSMemory()
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = filemanager.WithDefault(ctx, thisworkingPath, thisTempPath, UserID, GroupID)
+	urlTestHistoryStorage := urltest.NewHistoryStorage()
+	ctx = service.ContextWithPtr(ctx, urlTestHistoryStorage)
+	instance, err := SingBox.New(SingBox.Options{
+		Context: ctx,
+		Options: options,
+	})
+
+	if err != nil {
+		cancel()
+		return nil, Ex.Cause(err, "error creating sing-box service")
+	}
+
+	runtimeDebug.FreeOSMemory()
+	service := libbox.NewBoxService(
+		ctx,
+		cancel,
+		instance,
+		service.FromContext[pause.Manager](ctx),
+		urlTestHistoryStorage,
+	)
+	return &service, nil
+}
+
+func readOptions(configContent string) (option.Options, error) {
+	var options option.Options
+	err := options.UnmarshalJSON([]byte(configContent))
+	if err != nil {
+		return option.Options{}, Ex.Cause(err, "error decoding config")
+	}
+	return options, nil
 }
