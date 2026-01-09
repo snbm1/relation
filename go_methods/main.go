e0b57ec1b2a1886349aec751b0933720c79714d4
ZennardOS <shadowkod2006@gmail.com>
2026-01-09 14:50:19 +0300

    completed setup and errorOrNot methods

--- go_methods/main.go
+++ go_methods/main.go
@@ -1,10 +1,24 @@
 package main
 
+import "C"
 import (
-	"fmt"
+	_ "fmt"
+	V2 "methods/V2"
 	_ "methods/manager"
+
+	"github.com/sagernet/sing-box/log"
 )
 
-func main() {
-	fmt.Println("hello")
+//export setup
+func setup(basicPath, workingPath, tempPath *C.char, statusPort C.longlong, debug, enableservices bool) *C.char {
+	err := V2.Setup(C.GoString(basicPath), C.GoString(workingPath), C.GoString(tempPath), int64(statusPort), debug, enableservices)
+	return errorOrNot(err)
+}
+
+func errorOrNot(err error) *C.char {
+	if err == nil {
+		return C.CString("")
+	}
+	log.Error(err.Error())
+	return C.CString(err.Error())
 }
