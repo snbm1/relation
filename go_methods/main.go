6aa4377d8bea1cd4633aca6dc170a67ec054b4ab
ZennardOS <shadowkod2006@gmail.com>
2026-01-23 14:28:29 +0300

    finishing parse method

--- go_methods/main.go
+++ go_methods/main.go
@@ -5,6 +5,7 @@ import (
 	_ "fmt"
 	V2 "methods/V2"
 	_ "methods/manager"
+	rb "methods/relationrpc"
 
 	"github.com/sagernet/sing-box/log"
 )
@@ -13,6 +14,27 @@ import (
 func setup(basicPath, workingPath, tempPath *C.char, statusPort C.longlong, debug, enableservices bool) *C.char {
 	err := V2.Setup(C.GoString(basicPath), C.GoString(workingPath), C.GoString(tempPath), int64(statusPort), debug, enableservices)
 	return errorOrNot(err)
+}
+
+//export parse
+func parse(content *C.char, tempPath *C.char, debug bool) *C.char {
+	res, err := V2.Parse(&rb.ParseRequest{
+		Content:  C.GoString(content),
+		TempPath: C.GoString(tempPath),
+		Debug:    debug,
+	})
+
+	if err != nil {
+		log.Error(err.Error())
+		return C.CString(err.Error())
+	}
+
+	if res.ResponceFlag != rb.ResponseFlag_OK {
+		return C.CString(res.Message)
+	}
+
+	return C.CString(res.Content)
+
 }
 
 func errorOrNot(err error) *C.char {
