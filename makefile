26f65032a66b3bd6b4ac26183caea07ea557d5c6
snbm1 <1pro100me1@gmail.com>
2026-02-22 17:07:56 +0300

    Add setting file, add field current config, edit makefile

--- makefile
+++ makefile
@@ -1,22 +1,25 @@
-# var names 
 PROJECT := methods 
-OUT := build
 LIB_NAME := librelation.so
 SRC_DIR := go_methods
 
-#Sing-Box tags 
 SINGBOX_TAGS := with_grpc with_dhcp with_quic with_utls with_acme with_gvisor with_tailscale
 TAGS := linux $(SINGBOX_TAGS)
 
-build: 
+clean: 
+	rm librelation.*
+	rm relation
+
+build: clean
 	CGO_ENABLED=1 go build -tags "$(TAGS)" -buildmode=c-shared -o ./$(LIB_NAME) ./$(SRC_DIR)
+	cargo build
+	cp target/debug/relation .
 
 
-release: 
+release: clean
 	CGO_ENABLED=1 go build -tags "$(TAGS)" -ldflags "-s -w" -buildmode=c-shared -o ./$(LIB_NAME) ./$(SRC_DIR)
+	cargo build --release
+	cp target/release/relation .
 
 test: 
 	go test -tags "$(TAGS)" ./... -v 
 
-clean: 
-	rm -fr $(OUT)
--- src/datamanager.rs
+++ src/datamanager.rs
@@ -1,17 +1,71 @@
 use directories::ProjectDirs;
-use std::fs;
-use std::path::{PathBuf};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fs::{self, File};
+use std::path::PathBuf;
+use toml;
 
+use crate::bridge;
 use crate::configurator::Configurator;
 
-pub struct DataManager {
-    data_dir: PathBuf,
-    configs: Vec<String>,
-    handler: Configurator,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Settings {
+    current: Option<String>,
+    unable_system_proxy: Option<bool>,
 }
 
-impl DataManager {
+impl Settings {
+    pub fn new(setting_file: PathBuf) -> Result<Self, Box<dyn Error>> {
+        // Try to read the file, if it doesn't exist, create default settings and save
+        match fs::read_to_string(&setting_file) {
+            Ok(content) => Ok(toml::from_str(&content)?),
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                let settings = Self {
+                    current: None,
+                    unable_system_proxy: None,
+                };
 
+                settings.save(setting_file)?;
+                Ok(settings)
+            }
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+
+    pub fn save(&self, setting_file: PathBuf) -> Result<(), Box<dyn Error>> {
+        if let Some(parent) = setting_file.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        let tmp_file = setting_file.with_extension("tmp");
+        let toml_string = toml::to_string_pretty(self)?;
+
+        fs::write(&tmp_file, toml_string)?;
+        fs::rename(tmp_file, setting_file)?;
+
+        Ok(())
+    }
+
+    pub fn read(&mut self, setting_file: PathBuf) -> Result<(), Box<dyn Error>> {
+        match fs::read_to_string(&setting_file) {
+            Ok(content) => {
+                *self = toml::from_str(&content)?;
+                Ok(())
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(()),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+}
+
+pub struct App {
+    data_dir: PathBuf,
+    configs: Vec<String>,
+    cfg_handler: Configurator,
+    stg_handler: Settings,
+}
+
+impl App {
     pub fn new(app_name: &str) -> Self {
         let proj_dirs = ProjectDirs::from("", "", app_name)
             .expect("[ERROR] Unable to determine project directories");
@@ -25,7 +79,8 @@ impl DataManager {
         let mut mng = Self {
             data_dir,
             configs: vec![],
-            handler: Configurator::new(),
+            cfg_handler: Configurator::new(),
+            stg_handler: Settings::new(settings_file).unwrap(),
         };
 
         mng.configs = mng.read_configs();
@@ -35,14 +90,14 @@ impl DataManager {
     pub fn read_configs(&mut self) -> Vec<String> {
         let mut result = Vec::new();
 
-        let entries = std::fs::read_dir(&self.get_configs_path())
+        let entries = std::fs::read_dir(self.get_configs_path())
             .expect("[ERROR] Failed to read config directory");
 
         for entry in entries {
             let entry = entry.expect("[ERROR] Failed to read directory entry");
             let path = entry.path();
 
-            if path.is_file() && path.extension().unwrap() == "json" {
+            if path.is_file() && path.extension().unwrap_or_default() == "json" {
                 if let Some(stem) = path.file_stem() {
                     result.push(stem.to_str().unwrap().to_string());
                 }
@@ -69,7 +124,9 @@ impl DataManager {
     }
 
     pub fn remove_config_by_number(&mut self, number: usize) {
-        let file_path = self.get_configs_path().join(format!("{}.json", self.configs[number]));
+        let file_path = self
+            .get_configs_path()
+            .join(format!("{}.json", self.configs[number]));
 
         if !file_path.exists() {
             panic!(
@@ -84,18 +141,69 @@ impl DataManager {
     }
 
     pub fn add_config(&mut self) -> &mut Self {
-        self.configs
-            .push(self.handler.save_to_file(self.get_configs_path()).unwrap());
+        self.configs.push(
+            self.cfg_handler
+                .save_to_file(self.get_configs_path())
+                .unwrap(),
+        );
         self.configs.sort();
         self
     }
 
+    pub fn run_app(&mut self, tag: Option<String>, number: Option<u16>, unable_system_proxy: bool) {
+        self.stg_handler.read(self.get_settings_path());
+
+        let file_path;
+        if let Some(n) = tag {
+            file_path = self.get_configs_path().join(format!("{}.json", n));
+            self.stg_handler.current = Some(n);
+        } else if let Some(n) = number {
+            file_path = self
+                .get_configs_path()
+                .join(format!("{}.json", self.get_list()[n as usize - 1]));
+            self.stg_handler.current = Some(self.get_list()[n as usize - 1].clone());
+        } else if let Some(n) = self.stg_handler.current.clone() {
+            file_path = self.get_configs_path().join(format!("{}.json", n));
+        } else {
+            file_path = self
+                .get_configs_path()
+                .join(format!("{}.json", self.get_list()[0]));
+            self.stg_handler.current = Some(self.get_list()[0].clone());
+        }
+
+        bridge::start_safe(file_path.to_str().unwrap(), 0);
+        if unable_system_proxy {
+            self.stg_handler.unable_system_proxy = Some(unable_system_proxy);
+        }
+
+        if !unable_system_proxy {
+            if self.handler_ref().get_list_of_system_proxies().len() > 1 {
+                panic!("[ERROR] A more than 1 system proxy");
+            } else if let Some((host, port, support_socks)) =
+                self.handler_ref().get_list_of_system_proxies().first()
+            {
+                bridge::enable_system_proxy_safe(host, *port as i64, *support_socks);
+            }
+        }
+        let _ = self.stg_handler.save(self.get_settings_path());
+        println!("[INFO] Run Relation");
+    }
+
+    pub fn stop_app(&mut self) {
+        if !self.stg_handler.unable_system_proxy.unwrap_or(true) {
+            bridge::disable_system_proxy_safe();
+        }
+        bridge::stop_safe();
+        let _ = self.stg_handler.save(self.get_settings_path());
+        println!("[INFO] Shutdown Relation");
+    }
+
     pub fn handler_ref(&self) -> &Configurator {
-        &self.handler
+        &self.cfg_handler
     }
 
     pub fn handler_mut(&mut self) -> &mut Configurator {
-        &mut self.handler
+        &mut self.cfg_handler
     }
 
     pub fn get_data_path(&self) -> PathBuf {
