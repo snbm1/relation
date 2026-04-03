c1a1c3dca8d088bf7c2c4548808d7fd8c82956f9
snbm <1pro100me1@gmail.com>
2026-04-03 13:28:32 +0300

    add Requst-Response structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,33 @@ mod bridge;
 mod configurator;
 pub mod ui;
 
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+pub const SOCKET_NAME: &str = "relation.sock";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Request {
+    pub command: Command,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Command {
+    Status,
+    Start { config_path: String },
+    Stop,
+    EnableSysProxy,
+    DisableSysProxy,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum StatusResponse {
+    Running,
+    Stopped,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Response {
+    pub reply: Result<(), String>,
+}
