0a13072fbcf01e1fd142341f1f20510cc36d7a10
snbm <1pro100me1@gmail.com>
2026-04-28 16:47:51 +0300

    stupid refactor

--- src/ui/tui/minireq.rs
+++ src/ui/tui/minireq.rs
@@ -3,7 +3,7 @@ use std::{
     io::{Read, Write},
     net::TcpStream,
 };
-//This is realisation of mini ureq 
+//This is realisation of mini ureq
 pub fn parse_status_code(status: &str) -> Result<u16, Box<dyn Error>> {
     let mut parts = status.split_whitespace();
     let _http_ver = parts.next().ok_or("Missing HTTP version")?;
@@ -20,26 +20,24 @@ pub fn send_http_request(addr: &str, request: &str) -> Result<String, Box<dyn Er
     Ok(response)
 }
 
-// Don't remember adding auto port func 
+// Don't remember adding auto port func
 pub fn get_ip(proxy: Option<&str>) -> Result<String, Box<dyn Error>> {
     let (addr, request) = match proxy {
         None => {
-            let request =
-                "GET / HTTP/1.1\r\n\
+            let request = "GET / HTTP/1.1\r\n\
                  Host: api.ipify.org\r\n\
                  Connection: close\r\n\
                  \r\n"
-                    .to_string();
+                .to_string();
 
             ("api.ipify.org:80".to_string(), request)
         }
         Some(proxy_addr) => {
-            let request =
-                "GET http://api.ipify.org/ HTTP/1.1\r\n\
+            let request = "GET http://api.ipify.org/ HTTP/1.1\r\n\
                  Host: api.ipify.org\r\n\
                  Connection: close\r\n\
                  \r\n"
-                    .to_string();
+                .to_string();
 
             (proxy_addr.to_string(), request)
         }
@@ -56,9 +54,8 @@ pub fn get_ip(proxy: Option<&str>) -> Result<String, Box<dyn Error>> {
         return Err(format!("Server response unsuccessful: {status_code}").into());
     }
 
-    let (_, body) = rest
-        .split_once("\r\n\r\n")
-        .ok_or("Can't get body")?;
+    let (_, body) = rest.split_once("\r\n\r\n").ok_or("Can't get body")?;
 
     Ok(body.trim().to_string())
 }
+
